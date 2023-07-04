fd6872863c3763719309106906a0faa622fa5852
Guilherme-RSM <m.guilherme3@gmail.com>
2023-07-04 15:21:04 -0300

    Iniciando estudo de vetores unidimensionais

---
+++ studies/Algorítimos em C++/Vetores unidimencionais/Vetores unidimencionais.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	int notas[4];
+	cout <<"Digite as suas notas dos 4 bimestre \n";
+	for(int i=0; i<=3; i++){
+		cin >> notas[i];
+	}
+	
+	
+	
+}
